973f7cc40730a4ffd574ce30ede1046d7aa9245d
Rodrigo Guimarães Araújo <rodrigoara27@gmail.com>
2018-10-18 21:17:11 -0300

    começando a fazer o chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,28 @@
 corpus = {
-    ''
+    'DOCUMENTOS_LEVAR':[
+        'O que levo pra votar?',
+        'Levo que documentos pra votar?'
+        'Pode votar com a carteira de motorista?'
+        'Vota sem documento?'
+        'O etitulo é válido pra votar?'
+    ],
+    'CANDIDATOS_VOTAR':[
+        'Quantos candidatos vou votar?',
+        'Esse ano vota pra presidente?'
+        'Vou votar em quantos senadores?'
+        'Voto em que esse ano?'
+        'Vai ter uma lista de candidatos?'
+    ],'DOCUMENTOS_LEVAR':[
+        'O que levo pra votar?',
+        'Levo que documentos pra votar?'
+        'Pode votar com a carteira de motorista?'
+        'Vota sem documento?'
+        'O etitulo é válido pra votar?'
+    ],'DOCUMENTOS_LEVAR':[
+        'O que levo pra votar?',
+        'Levo que documentos pra votar?'
+        'Pode votar com a carteira de motorista?'
+        'Vota sem documento?'
+        'O etitulo é válido pra votar?'
+    ]
 }
