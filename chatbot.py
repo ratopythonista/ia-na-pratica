17a81fa720691d7a92c724553a5f7e01198c0e9c
Rodrigo Guimarães Araújo <rodrigoara27@gmail.com>
2018-10-18 22:09:04 -0300

    chatbot pronto

--- chatbot.py
+++ chatbot.py
@@ -1,28 +1,25 @@
-corpus = {
-    'DOCUMENTOS_LEVAR':[
-        'O que levo pra votar?',
-        'Levo que documentos pra votar?'
-        'Pode votar com a carteira de motorista?'
-        'Vota sem documento?'
-        'O etitulo é válido pra votar?'
-    ],
-    'CANDIDATOS_VOTAR':[
-        'Quantos candidatos vou votar?',
-        'Esse ano vota pra presidente?'
-        'Vou votar em quantos senadores?'
-        'Voto em que esse ano?'
-        'Vai ter uma lista de candidatos?'
-    ],'DOCUMENTOS_LEVAR':[
-        'O que levo pra votar?',
-        'Levo que documentos pra votar?'
-        'Pode votar com a carteira de motorista?'
-        'Vota sem documento?'
-        'O etitulo é válido pra votar?'
-    ],'DOCUMENTOS_LEVAR':[
-        'O que levo pra votar?',
-        'Levo que documentos pra votar?'
-        'Pode votar com a carteira de motorista?'
-        'Vota sem documento?'
-        'O etitulo é válido pra votar?'
-    ]
-}
+import imp
+import json
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+json_data = open('corpus_eleicao.json').read()
+corpus = json.loads(json_data)
+
+intencoes_treino, perguntas_treino = list(), list()
+for intencao in corpus:
+    for pergunta in corpus[intencao]:
+        intencoes_treino.append(intencao)
+        perguntas_treino.append(pergunta)
+
+vocabulario = TfidfVectorizer().fit(perguntas_treino)
+perguntas_tokenizadas = vocabulario.transform(perguntas_treino)
+
+modelo = SVC(probability=True)
+modelo.fit(perguntas_tokenizadas, intencoes_treino)
+
+while True:
+    entrada = input()
+    entrada_tokenizada = vocabulario.transform([entrada])
+    print(modelo.predict(entrada_tokenizada))
